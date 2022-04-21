26a103a8fd4b37aad356a1a8db16d64cbe654592
TymsterBTW <88202578+TymsterBTW@users.noreply.github.com>
2022-04-21 17:27:55 +0100

    pushing cause y not

--- Sites/Home/script.js
+++ Sites/Home/script.js
@@ -4,26 +4,48 @@ const percentage_total = document.getElementById('percentage-total')
 const percentage_output = document.getElementById('percentage-output')
 const percentage_change = document.getElementById('percentage-change')
 const percentage_pure = document.getElementById('percentage-pure')
+const Percentage_score_total = document.getElementById('percentage-score-total')
+const percentage_input = document.getElementById('percentage-input')
+const percentage_error = document.getElementById('percentage-error')
 //percenage formulas
-var percentage_display = "score-total"
+var button_change = 1
 percentage_total.oninput = () =>{
     percentage()
-    percentage_display = "score-total"
-    percentage_score.setAttribute("max" , percentage_total.value)
 }
 percentage_score.oninput = () =>{
     percentage()
 }
-percentage_score.oninput =() =>{
+percentage_pure.oninput =() =>{
     percentage()
-    percentage_display = "pure"
 }
+percentage_change.addEventListener("click", () =>{
+    percentage_output.innerHTML = ""
+    if (button_change == 1){
+        button_change = 2
+        percentage_change.innerHTML = "Press to enter score and total"
+        Percentage_score_total.classList.add("hidden")
+        percentage_input.classList.remove("hidden")
+    }
+    else{
+        button_change = 1
+        Percentage_score_total.classList.remove("hidden")
+        percentage_input.classList.add("hidden")
+    }
+})
 
 function percentage(){
-    if (percentage_display == "score-total"){
-        percentage_output.innerHTML = percentage_score.value / percentage_total.value * 100
+    if (button_change == 1){
+        percentage_output.innerHTML = (percentage_score.value / percentage_total.value * 100).toFixed(2)
+        console.log(percentage_score)
+        console.log(percentage_total)
+        if (percentage_score.value < percentage_total.value){
+            console.log("higher")
+        }
+        else {
+            console.log("else")
+        }
     }
     else {
-        percentage_output.innerHTML = percentage_pure
+        percentage_output.innerHTML = percentage_pure.value
     }
 }
